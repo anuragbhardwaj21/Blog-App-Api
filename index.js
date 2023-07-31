89549460a819f637b264ea7dbe3bd1dea9a194b4
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-07-31 22:33:00 +0530

    master

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-const mongoURI = "mongodb+srv://anurag2361:anuraggg@anurag2361.1pepyj9.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI =
+  "mongodb+srv://anurag2361:anuraggg@anurag2361.1pepyj9.mongodb.net/?retryWrites=true&w=majority";
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
 app.use(cors());
 app.use(express.json());
-const uRoutes = require('./userRoutes');
-const bRoutes = require('./blogRoutes');
+const uRoutes = require("./userRoutes");
+const bRoutes = require("./blogRoutes");
 app.use(uRoutes);
 app.use(bRoutes);
-const port = 8000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Port ${port}`));
