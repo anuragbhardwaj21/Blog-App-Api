5c367100b7b87c634999f6420c5fcdc7406bda0f
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-08-01 01:19:09 +0530

    master

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-app.use(cors({ origin: "http://localhost:3000/"}));
+app.use(cors({ origin: true }));
 
 app.options("*", cors());
 
