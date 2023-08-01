c8861686d214b302c508d51a5ef6f1c229d0dc72
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-08-01 10:32:22 +0530

    master

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-app.use(cors());
-
+app.use(cors({ origin: 'http://localhost:3000' }));
 const mongoURI =
   "mongodb+srv://anurag2361:anuraggg@anurag2361.1pepyj9.mongodb.net/?retryWrites=true&w=majority";
 mongoose
@@ -17,5 +16,6 @@ const bRoutes = require("./blogRoutes");
 const router = require("./userRoutes");
 app.use(uRoutes);
 app.use(bRoutes);
+app.use(cors({ origin: 'http://localhost:3000' }));
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Port ${port}`));
