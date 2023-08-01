538c72f5a12bdba3507ce44a8ad9bd4a569678ca
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-08-01 09:56:58 +0530

    master

--- userRoutes.js
+++ userRoutes.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/User');
-const authMiddleware = require('./authMiddleware');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
+const authMiddleware = require("./authMiddleware");
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", cors(), async (req, res) => {
   try {
     const { name, email, password, age, phoneNumber } = req.body;
     const hP = await bcrypt.hash(password, 10);
@@ -17,21 +18,21 @@ router.post('/signup', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (!user) {
-      throw new Error('User not found');
+      throw new Error("User not found");
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
-      throw new Error('Invalid login credentials');
+      throw new Error("Invalid login credentials");
     }
 
-    const token = jwt.sign({ _id: user._id.toString() }, 'anurag');
+    const token = jwt.sign({ _id: user._id.toString() }, "anurag");
     user.tokens = user.tokens.concat({ token });
     await user.save();
     res.send({ user, token });
@@ -40,9 +41,11 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', authMiddleware, async (req, res) => {
+router.post("/logout", authMiddleware, async (req, res) => {
   try {
-    req.user.tokens = req.user.tokens.filter(tokenObj => tokenObj.token !== req.token);
+    req.user.tokens = req.user.tokens.filter(
+      (tokenObj) => tokenObj.token !== req.token
+    );
     await req.user.save();
     res.send();
   } catch (error) {
@@ -50,7 +53,7 @@ router.post('/logout', authMiddleware, async (req, res) => {
   }
 });
 
-router.post('/logoutAll', authMiddleware, async (req, res) => {
+router.post("/logoutAll", authMiddleware, async (req, res) => {
   try {
     req.user.tokens = [];
     await req.user.save();
